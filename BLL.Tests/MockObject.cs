1743606efcc6cb5df7503078c526ce712892b613
Domino <zhikhanastasya@gmail.com>
2018-10-26 18:48:10 +0300

    Add file

---
+++ BLL.Tests/MockObject.cs
@@ -0,0 +1,48 @@
+﻿using BLL.Interfaces.Cache;
+using BLL.Interfaces.DTO;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL.Tests
+{
+    public static class MockObject
+    {
+        public static ICacheDictionaty<T> CreateCacheDictionary<T>(List<CacheItem<T>> data)
+        {
+            var mock = new Mock<ICacheDictionaty<T>>();
+
+            mock.Setup(x => x.Add(It.IsAny<CacheItem<T>>()))
+                .Callback(new Action<CacheItem<T>>(x =>
+                {
+                    data.Add(x);
+                }))
+                .Returns(true);
+
+            mock.Setup(x => x.Update(It.IsAny<CacheItem<T>>()))
+                .Callback(new Action<CacheItem<T>>(x =>
+                {
+                    var i = data.FindIndex(q => q.Key.Equals(x.Key));
+                    data[i] = x;
+                }));
+
+            mock.Setup(x => x.Delete(It.IsAny<string>()))
+                .Returns((string key) =>
+                {
+                    var item = data.Find(q => q.Key.Equals(key));
+                    return data.Remove(item);
+                });
+
+            mock.Setup(x => x.Get(It.IsAny<string>()))
+                .Returns((string key) => data.FirstOrDefault(
+                x => x.Key == key));
+
+            mock.Setup(x => x.IsExists(It.IsAny<string>()))
+                .Returns((string key) => data.Exists(
+                x => x.Key == key));
+
+            return mock.Object;
+        }
+    }
+}
