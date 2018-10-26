9d29dd3427be538ee25a46d644e96e7a26dfc676
Domino <zhikhanastasya@gmail.com>
2018-10-26 11:21:30 +0300

    Add file

---
+++ BLL.Tests/Internals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Tests
+{
+    internal static class Internals
+    {
+        public const int INT_INIT_VALUE = 1234;
+        public const string KEY_FOR_INT_INIT_VALUE = "testKey";
+    }
+}
